6076bed84db5953f3c0297562fb3e935d87b6da6
technopolis.team <antonym80@gmail.com>
2024-06-24 00:41:33 +0300

    initial commit

---
+++ AvtoTransApp/Models/FrameObj.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Controls;
+
+namespace AvtoTransApp.Models
+{
+    public class FrameObj
+    {
+        public static Frame MainFrame;
+    }
+}
